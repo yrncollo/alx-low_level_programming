ac61ea860c87d0723062eb8d62372c5936f6d616
YrnCollo <kngenocollins@gmail.com>
2022-06-02 20:01:06 +0300

    C program that prints using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char output[] = "\"Programming is like building a multilingual puzzle\n";
+	puts(output);
+	return 0;
+}
