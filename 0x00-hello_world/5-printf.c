9d826f19f12b56e079d130e30b7b2fe5dfe9c26a
YrnCollo <kngenocollins@gmail.com>
2022-06-02 21:32:20 +0300

    C program that prints using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hello[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", hello);
+	return (0);
+}
