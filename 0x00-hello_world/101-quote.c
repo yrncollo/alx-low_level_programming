0b85de026e4382a23cac32cb3b48bdf4d9a4ffe0
YrnCollo <kngenocollins@gmail.com>
2022-06-02 23:13:15 +0300

    C program that prints out without using printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char name[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(name, stdout);
+	return (0);
+}
