afe54f2950fc7dfc5fe28ff273311bd22cf79681
YrnCollo <kngenocollins@gmail.com>
2022-06-07 17:19:58 +0300

    unction that prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i,j;
+
+	for (j =0; j < 10; j++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+		{
+                _putchar(i);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
