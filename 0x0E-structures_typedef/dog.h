3b65a860e9560d0e681d717390b9d8ff679b2d00
YrnCollo <kngenocollins@gmail.com>
2022-07-05 00:39:17 +0300

    made changes to dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#ifndef _DOG_H_
 #define _DOG_H_
 
 /**
@@ -16,3 +17,5 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
