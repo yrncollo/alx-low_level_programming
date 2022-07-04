944b5f88e6dd29dc7bc6692d3556eb9a7bb41e81
YrnCollo <kngenocollins@gmail.com>
2022-07-05 00:15:29 +0300

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#define _DOG_H_
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Its structure to  define a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
